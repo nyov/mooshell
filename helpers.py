738152a608bb228aa60a0786a8a22018f63f80e3
Piotr Zalewa <piotr@zalewa.info>
2010-06-21 12:57:22 +0100

    logging delete cache

--- helpers.py
+++ helpers.py
@@ -7,6 +7,10 @@ def expire_page(path):
 	request = HttpRequest()
 	request.path = path
 	key = get_cache_key(request)
+	log_to_file('deleting cache: %s, path: %s' % (key, request.path))
 	if cache.has_key(key):   
 		cache.delete(key)
 		log_to_file('cache deleted %s, path: %s' % (key, request.path))
+	else:
+		log_to_file('no such key' % key)
+	
