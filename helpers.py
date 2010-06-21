7b69d2114a37dd27b7df016e4fc8bbe336408cda
Piotr Zalewa <piotr@zalewa.info>
2010-06-21 12:55:42 +0100

    logging delete cache

--- helpers.py
+++ helpers.py
@@ -1,6 +1,7 @@
 from django.core.cache import cache
 from django.http import HttpRequest
 from django.utils.cache import get_cache_key
+from base.utils import log_to_file, separate_log
 
 def expire_page(path):
 	request = HttpRequest()
@@ -8,3 +9,4 @@ def expire_page(path):
 	key = get_cache_key(request)
 	if cache.has_key(key):   
 		cache.delete(key)
+		log_to_file('cache deleted %s, path: %s' % (key, request.path))
