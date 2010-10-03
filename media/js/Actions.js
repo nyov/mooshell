3062a45b052697214eb54ad536d345c124174b70
Piotr Zalewa <piotr@zalewa.info>
2010-10-03 03:06:11 +0100

    placeholder class added

--- media/js/Actions.js
+++ media/js/Actions.js
@@ -1,3 +1,45 @@
+// doesn't clear the message in IE6 - help?
+
+(function() {
+
+this.InputPlaceholder = new Class({
+    Implements: Options,
+    options: {
+        placeholderClass: 'placeholder'
+    },
+    initialize: function(element, options) {
+
+        if ('placeholder' in document.createElement('input')) return;
+
+        this.setOptions(options);
+        this.element = $(element);
+        this.labelText = this.element.get('placeholder');
+        var self = this;
+        var clear = function() {
+            if (self.element.get('value') === self.labelText) {
+                self.element.set('value', '');
+                self.element.removeClass(self.options.placeholderClass);
+            }
+        };
+        var set = function() {
+            if (!self.element.get('value')) {
+                self.element.set('value', self.labelText);
+                self.element.addClass(self.options.placeholderClass);
+            }
+        };
+        this.element.addEvents({
+            focus: clear,
+            blur: set
+        });
+        var form = this.element.getParent('form');
+        console.log(form);
+        if (form) form.addEvent('submit', function() {clear(), alert('xxx');});
+        set();
+    }
+});
+
+})();
+
 // TODO: refactor 
 var DiscussionWindow = new Class({
 	initialize: function(){
