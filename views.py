b3296db1df2f0b8dba09c932ba46b7d6f66131ba
Piotr Zalewa <piotr@zalewa.info>
2011-01-22 10:51:06 +0000

    logging get_cache_key if author is watching

--- views.py
+++ views.py
@@ -46,7 +46,8 @@ def get_pastie_edit_key(req, slug=None, version=None, revision=None,
     if revision: key = "%s:%d" % (key, revision)
     if author: key = "%s:%s" % (key, author)
     if skin: key = "%s:%s" % (key, skin)
-
+    if author == req.user.username:
+        log_to_file("DEBUG: get_pastie_edit_key: %s" % key)
     return key
 
 def pastie_edit(req, slug=None, version=None, revision=None, author=None,
